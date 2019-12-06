6478f30287383a32ef906be40c2e312aeb209380
vadimg7 <vadim.golovanov@gmail.com>
2019-12-06 13:01:29 -0600

    Adding Test Suit

---
+++ Test Suites/TS_RegressionTest.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_RegressionTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1a6c512a-86e7-45b4-b866-ee7ad3ae5aed</testSuiteGuid>
+   <testCaseLink>
+      <guid>baaadee0-ed9d-4284-b83c-2c8bc4a7aa19</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/navigate back and forward</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
